ae55b3a8570e03e4c5f035a4ebc861b75c93b109
Nemanja Vasic <nemanjavasa@gmail.com>
2017-10-23 16:27:24 +0200

    Commited

---
+++ src/test/java/guru/springframework/springrestclientexamples/services/ApiServiceImplTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.springrestclientexamples.services;
+
+import static org.junit.Assert.*;
+import guru.springframework.api.domain.User;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ApiServiceImplTest {
+
+	@Autowired
+	ApiService apiService;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetUsers() {
+		List<User> users = apiService.getUsers(3);
+		assertEquals(3, users.size());
+	}
+
+}
